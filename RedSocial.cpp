57a0f3b84a35a15ee15e00926e48936921f668d2
goyooo <133908348+goyooo@users.noreply.github.com>
2025-11-20 08:48:36 -0300

    redsocial

--- RedSocial.cpp
+++ RedSocial.cpp
@@ -1,155 +1,40 @@
-#include "RedSocial.h"
-#include <iostream>
+#ifndef __REDSOCIAL_H__
+#define __REDSOCIAL_H__
+
+#include <string>
 #include <map>
+#include <unordered_map>
 #include <set>
-#include <vector>
 #include <string>
 using namespace std;
+class RedSocial{
+  public:
+    RedSocial();
 
-RedSocial::RedSocial():amistades_count(0){
-}
+    const std::set<int> & usuarios() const; // O(1)
+    std::string obtener_alias(int id) const; // O(log n)
+    const std::set<std::string> & obtener_amigos(int id) const; // O(log n)
+    const std::set<std::string> & obtener_conocidos(int id) const; // O(log n)
+    int cantidad_amistades() const; // O(1)
 
-const set<int> & RedSocial::usuarios() const{
-    return this->ids;
-}
-string RedSocial::obtener_alias(int id) const{
-    return this->users.at(id);
-}
-const set<string> & RedSocial::obtener_amigos(int id) const{
-    return amigos.at(id);
-}
-
-const set<string> & RedSocial::obtener_conocidos(int id) const{  
-    return conocidos.at(id);
-}
-
-int RedSocial::cantidad_amistades() const{
-    return this->amistades_count;
-}
-
-void RedSocial::registrar_usuario(string alias, int id){
-    users[id] = alias;
-    ids.insert(id);
-    amigos[id] = {};
-    alias_to_id[alias] = id;
-    conocidos[id] = {};
-}
-
-void RedSocial::amigar_usuarios(int id_A, int id_B){
-    string alias_A = users[id_A];
-    string alias_B = users[id_B];
-    amigos[id_A].insert(alias_B);
-    amigos[id_B].insert(alias_A);
-    this->amistades_count += 1;  
-
-    //A y B ya no pueden ser conocidos
-    conocidos[id_A].erase(alias_B);
-    conocidos[id_B].erase(alias_A);
-
-        for(auto amigo_de_B : amigos[id_B]){
-        if(amigo_de_B != alias_A){
-            int id_amigo_de_B = obtener_id(amigo_de_B);
-
-            // Si Y no es amigo directo de A, entonces son conocidos
-            if(amigos[id_A].count(amigo_de_B) == 0){
-                conocidos[id_A].insert(amigo_de_B);
-                conocidos[id_amigo_de_B].insert(alias_A);
-            }
-        }
-    }
-
-    // 4. Actualizar conocidos de B con los amigos de A
-    for(auto amigo_de_A : amigos[id_A]){
-        if(amigo_de_A != alias_B){
-            int id_amigo_de_A = obtener_id(amigo_de_A);
-
-            // Si X no es amigo directo de B, entonces son conocidos
-            if(amigos[id_B].count(amigo_de_A) == 0){
-                conocidos[id_B].insert(amigo_de_A);
-                conocidos[id_amigo_de_A].insert(alias_B);
-            }
-        }
-    }
-}
-
-/// chatgepetie esta funcion, banco mucho la idea pero no tuve tiempo de verla  
-void RedSocial::reconstruir_conocidos_de(int id_u) {
-    const string alias_u = users[id_u];
-    auto& out = conocidos[id_u];
-    out.clear();
-    // Por cada amigo f de u...
-    for (const auto& alias_f : amigos[id_u]) {
-        int id_f = obtener_id(alias_f);
-        // ...agrego los amigos de f como conocidos de u
-        for (const auto& alias_w : amigos[id_f]) {
-            if (alias_w != alias_u && amigos[id_u].count(alias_w) == 0) {
-                out.insert(alias_w);
-            }
-        }
-    }
-}
-
-void RedSocial::desamigar_usuarios(int id_A, int id_B){
-    string alias_A = users[id_A];
-    string alias_B = users[id_B];
-
-    // Guardar amigos previos a cortar
-    auto amigos_A_antes = amigos[id_A];
-    auto amigos_B_antes = amigos[id_B];
-
-    // Cortar amistad
-    amigos[id_A].erase(alias_B);
-    amigos[id_B].erase(alias_A);
-    amistades_count -= 1;
-
-    // Conjunto de afectados a reconstruir: A, B, sus amigos previos
-    set<int> afectados;
-    afectados.insert(id_A);
-    afectados.insert(id_B);
-    for (const auto& aliasX : amigos_A_antes) afectados.insert(obtener_id(aliasX));
-    for (const auto& aliasY : amigos_B_antes) afectados.insert(obtener_id(aliasY));
-
-    // Reconstrucción local de conocidos
-    for (int id : afectados) {
-        reconstruir_conocidos_de(id);
-    }
-
-}
-
-void RedSocial::eliminar_usuario(int id){
-    vector<string> amigos_a_eliminar;
-    for(auto amigo : amigos[id]){
-        amigos_a_eliminar.push_back(amigo);
-    }
-    for(auto keys : amigos_a_eliminar){
-        desamigar_usuarios(id, obtener_id(keys));
-    }
-    string alias = users[id];
-    alias_to_id.erase(alias);
-    users.erase(id);
-    ids.erase(id);
-    amigos.erase(id);
-    conocidos.erase(id);
-}
-
-
-
-
-int RedSocial::obtener_id(string alias) const{
-    return alias_to_id.at(alias);
-}
-
-const set<string> & RedSocial::conocidos_del_usuario_mas_popular() const{
+    void registrar_usuario(std::string alias, int id); // O(log n) + O(1) promedio
+    void eliminar_usuario(int id); // sin requerimiento
+    void amigar_usuarios(int id_A, int id_B); // sin requerimiento
+    void desamigar_usuarios(int id_A, int id_B); // sin requerimiento
     
-    int id_usuario = conocidos.rbegin()->first;
-    int compare = conocidos.rbegin()->second.size();
-    
-    for(auto ids : conocidos){
-        if(ids.first != id_usuario && ids.second.size() > compare){
-            id_usuario = ids.first;
-            compare = ids.second.size();
-        }
-    }
-    return conocidos.at(id_usuario);
-}
+    int obtener_id(std::string alias) const; // O(1) promedio
+    const std::set<std::string> & conocidos_del_usuario_mas_popular() const; // O(1)
 
+  private:
+  std::map<int, std::string> users;
+  std::set<int> ids;
+  std::map<int, std::set<std::string>> amigos;
+  
+  std::unordered_map<std::string, int> alias_to_id;
+  std::map<int, set<std::string>> conocidos;
+  int amistades_count;
+  
+  void reconstruir_conocidos_de(int id_u);
+};
+
+#endif
